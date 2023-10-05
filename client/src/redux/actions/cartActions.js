4649aa8e0dc7dd359a9029e4d500ed198c427e46
fiskryeziu <fiskryeziu@gmail.com>
2023-10-05 19:00:27 +0200

    fix: added localStorage functionality

--- client/src/redux/actions/cartActions.js
+++ client/src/redux/actions/cartActions.js
@@ -3,22 +3,24 @@ import axios from 'axios';
 
 
 const URL =  "https://mern-flipkart-clone.onrender.com";
-export const addToCart = (id, quantity) => async (dispatch) => {
+export const addToCart = (id, quantity) => async (dispatch,getState) => {
     try { 
         const { data } = await axios.get(`${URL}/product/${id}`);
 
         dispatch({ type: actionTypes.ADD_TO_CART, payload: { ...data, quantity } });
 
+        localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
     } catch (error) {
         dispatch({ type: actionTypes.ADD_TO_CART_ERROR, payload: error.message});
 
     }
 };
 
-export const removeFromCart = (id) => (dispatch) => {
+export const removeFromCart = (id) => (dispatch,getState) => {
     dispatch({
         type: actionTypes.REMOVE_FROM_CART,
         payload: id
     })
 
+    localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
 };
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -4,15 +4,25 @@ import {composeWithDevTools} from 'redux-devtools-extension';
 import {getProductDetailsReducer, getProductsReducer} from './reducers/productReducer';
 import thunk from 'redux-thunk';
 import {cartReducer} from './reducers/cartReducer'
+
+const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+
 const reducer = combineReducers({
     getProducts: getProductsReducer,
     getProductDetails: getProductDetailsReducer,
     cart: cartReducer
 })
 
+const initialState = {
+    cart: {
+        cartItems: cartItemsFromStorage
+    }
+}
+
 const middleware = [thunk]
 const store = createStore(
     reducer,
+    initialState,
     composeWithDevTools(applyMiddleware(...middleware))
 )
 
