01869b6b2c60161ff51557c87aba10ba03c8ab28
Sumonta Saha Mridul <61287791+Sumonta056@users.noreply.github.com>
2023-10-06 18:01:19 +0600

    Added Responsive Menu Bar #1

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,88 +1,98 @@
-import React,{useState} from 'react'
-import { AppBar, Toolbar, styled, Box, Typography, IconButton, Drawer, List } from '@mui/material';
-import Search from './Search';
-import CustomButtons from './CustomButtons';
-import { Menu } from '@mui/icons-material';
-import { Link } from 'react-router-dom';
-
+import React, { useState } from "react";
+import {
+  AppBar,
+  Toolbar,
+  styled,
+  Box,
+  Typography,
+  IconButton,
+  Drawer,
+  List,
+} from "@mui/material";
+import Search from "./Search";
+import CustomButtons from "./CustomButtons";
+import ResizedNav from "./ResizedNav";
+import { Menu } from "@mui/icons-material";
+import { Link } from "react-router-dom";
 
 const StyledHeader = styled(AppBar)`
-    background: #2874f0;
-    height: 55px;
-  `
+  background: #2874f0;
+  height: 55px;
+`;
 const Component = styled(Link)`
-      margin-left:12%;
-      line-height:0;
-      color: inherit;
-      text-decoration: none;
-  `
+  margin-left: 12%;
+  line-height: 0;
+  color: inherit;
+  text-decoration: none;
+`;
 const SubHeading = styled(Typography)`
-      font-size:10px;
-      font-style: italic;
-  `
-const PlusImage = styled('img')({
+  font-size: 10px;
+  font-style: italic;
+`;
+const PlusImage = styled("img")({
   width: 10,
   height: 10,
   marginLeft: 4,
-})
+});
 
 const MenuButton = styled(IconButton)(({ theme }) => ({
-  display: 'none',
-  [theme.breakpoints.down('md')]: {
-      display: 'block'
-  }
+  display: "none",
+  [theme.breakpoints.down("md")]: {
+    display: "block",
+  },
 }));
 
-const CustomButtonWrapper = styled('span')(({ theme }) => ({
-  margin: '0 3% 0 auto',
-  [theme.breakpoints.down('md')]: {
-    display: 'none'
-  }
+const CustomButtonWrapper = styled("span")(({ theme }) => ({
+  margin: "0 3% 0 auto",
+  [theme.breakpoints.down("md")]: {
+    display: "none",
+  },
 }));
 
 const Header = () => {
-  const logoURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png';
-  const subURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png';
+  const logoURL =
+    "https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png";
+  const subURL =
+    "https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png";
 
   const [open, setOpen] = useState(false);
 
   const handleClose = () => {
-      setOpen(false);
-  }
+    setOpen(false);
+  };
 
   const handleOpen = () => {
-      setOpen(true);
-  }
+    setOpen(true);
+  };
   const list = () => (
-    <Box  onClick={handleClose}>
-        <List>
-            <listItem button>
-                <CustomButtons />
-            </listItem>
-        </List>
+    <Box onClick={handleClose}>
+      <List>
+        <listItem button>
+          <ResizedNav />
+        </listItem>
+      </List>
     </Box>
-);
+  );
   return (
     <StyledHeader>
       <Toolbar style={{ minHeight: 55 }}>
-      <MenuButton
-                    color="inherit"
-                    onClick={handleOpen}
-                >
-                    <Menu />
-                </MenuButton>
+        <MenuButton color="inherit" onClick={handleOpen}>
+          <Menu />
+        </MenuButton>
 
-                <Drawer open={open} onClose={handleClose}>
-                    {list()}
-                </Drawer>
-        <Component to='/'>
+        <Drawer open={open} onClose={handleClose}>
+          {list()}
+        </Drawer>
+        <Component to="/">
           <img src={logoURL} alt="logo" style={{ width: 75 }} />
-          <Box style={{ display: 'flex' }}>
-            <SubHeading>Explore&nbsp;
-              <Box component='span' style={{ color: '#ffe500' }}>Plus</Box>
+          <Box style={{ display: "flex" }}>
+            <SubHeading>
+              Explore&nbsp;
+              <Box component="span" style={{ color: "#ffe500" }}>
+                Plus
+              </Box>
             </SubHeading>
             <PlusImage src={subURL} alt="sub-logo" />
-
           </Box>
         </Component>
         <Search />
@@ -91,7 +101,7 @@ const Header = () => {
         </CustomButtonWrapper>
       </Toolbar>
     </StyledHeader>
-  )
-}
+  );
+};
 
 export default Header;
