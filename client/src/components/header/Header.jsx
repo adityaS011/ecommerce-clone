61543287f6312415b3c60fd4362d822e00071ce9
Aditya Singh <93607531+adityaS011@users.noreply.github.com>
2023-11-18 17:28:40 +0530

    Update Header.jsx

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -51,7 +51,7 @@ const CustomButtonWrapper = styled("span")(({ theme }) => ({
 
 const Header = () => {
   const logoURL =
-    "https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png";
+    "HomeCart";
   const subURL =
     "https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png";
 
