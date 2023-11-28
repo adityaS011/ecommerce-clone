6bfe5c94f0a9c294397e20d0d02a127077d9fc68
Aditya Singh <93607531+adityaS011@users.noreply.github.com>
2023-11-28 17:56:38 +0530

    Update CustomButtons.jsx

--- client/src/components/header/CustomButtons.jsx
+++ client/src/components/header/CustomButtons.jsx
@@ -1,91 +1,75 @@
-import React, { useState, useContext } from 'react';
-
-import { Box, Typography, Badge, Button, styled } from '@mui/material';
-import { ShoppingCart } from '@mui/icons-material';
-
-import { Link } from 'react-router-dom';
-import { LoginContext } from '../../context/ContextProvider';
+import React from 'react';
+import { useState, useContext } from 'react';
 import { useSelector } from 'react-redux';
-
+import { Box, Typography,Badge,  Button, styled } from '@mui/material';
+import { ShoppingCart } from '@mui/icons-material';
+import LoginDialog from '../login/LoginDialog';
+import { Link } from 'react-router-dom';
+import { DataContext } from '../../context/DataProvider';
 import Profile from './Profile';
-import LoginDialog from '../Login/LoginDialog';
-
-const Container = styled(Link)(({ theme }) => ({
-    display: 'flex',
-    [theme.breakpoints.down('sm')]: {
-        display: 'block'
-    }
-}));
 
 const Wrapper = styled(Box)(({ theme }) => ({
-    margin: '0 3% 0 auto',
+    margin: '0 2% 0 auto',
     display: 'flex',
     '& > *': {
         marginRight: '40px !important',
-        textDecoration: 'none',
-        color: '#FFFFFF',
-        fontSize: 12,
+        fontSize: 16,
         alignItems: 'center',
-        [theme.breakpoints.down('sm')]: {
-            color: '#2874f0',
-            alignItems: 'center',
-            display: 'flex',
-            flexDirection: 'column',
-            marginTop: 10
-        }
+   
     },
-    [theme.breakpoints.down('sm')]: {
+    [theme.breakpoints.down('md')]: {
+        display: 'block',
+    }
+}));
+
+const Container = styled(Link)(({ theme }) => ({
+    textDecoration: 'none',
+    color: 'inherit',
+    display: 'flex',
+    [theme.breakpoints.down('md')]: {
         display: 'block'
     }
 }));
+const LoginButton = styled(Button)`
+color: #2874f0;
+background: #ffffff;
+text-transform: none;
+padding: 5px 40px;
+border-radius: 2px;
+box-shadow: none;
+font-weight: 600;
+height:32px;
 
-const LoginButton = styled(Button)(({ theme }) => ({
-    color: '#2874f0',
-    background: '#FFFFFF',
-    textTransform: 'none',
-    fontWeight: 600,
-    borderRadius: 2,
-    padding: '5px 40px',
-    height: 32,
-    boxShadow: 'none',
-    [theme.breakpoints.down('sm')]: {
-        background: '#2874f0',
-        color: '#FFFFFF'
-    }
-}));
+`
 
 
 const CustomButtons = () => {
-    
+    const { account, setAccount } = useContext(DataContext);
     const [open, setOpen] = useState(false);
-    const { account, setAccount } = useContext(LoginContext);
-
-    const cartDetails = useSelector(state => state.cart);
-    const { cartItems } = cartDetails;
-
-    const openDialog = () => {
+    const {cartItems} = useSelector(state => state.cart);
+    const openDialog =()=>{
         setOpen(true);
     }
-
     return (
         <Wrapper>
             {
-                account ? <Profile account={account} setAccount={setAccount} /> :
-                    <LoginButton variant="contained" onClick={() => openDialog()}>Login</LoginButton>
-                
+                account? <Profile account={account} setAccount={setAccount}/> :
+                <LoginButton variant="contained" onClick={()=> openDialog()}>Login</LoginButton>
+
             }
-            <Typography style={{ marginTop: 3, width: 135 }}>Become a Seller</Typography>
-            <Typography style={{ marginTop: 3 }}>More</Typography>
             
+            <Typography style={{marginTop: 3, width: 135}}>Become a seller</Typography>
+            <Typography style={{marginTop: 3}}>More</Typography>
+
             <Container to='/cart'>
-                <Badge badgeContent={cartItems?.length} color="secondary">
-                    <ShoppingCart />
+                <Badge badgeContent={cartItems?.length} color="secondary" style={{color: 'red'}}>
+                <ShoppingCart  style={{color: 'white'}} />
                 </Badge>
-                <Typography style={{ marginLeft: 10 }}>Cart</Typography>
+                <Typography style={{marginLeft:'5px'}}>Cart</Typography>
             </Container>
-            <LoginDialog open={open} setOpen={setOpen} setAccount={setAccount} />
+            <LoginDialog open={open} setOpen={setOpen}/>
         </Wrapper>
-    )
+    );
 }
 
 export default CustomButtons;
