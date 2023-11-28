57834f99f019937ba3244dc1149c66b57e12ce1b
Aditya Singh <93607531+adityaS011@users.noreply.github.com>
2023-11-28 17:59:39 +0530

    Update Home.jsx

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -9,7 +9,6 @@ import { getProducts } from '../../redux/actions/productAction';
 import { Box, styled } from '@mui/material';
 import MidSlide from "./MidSlide";
 import MidSection from './MidSection';
-import Contact from './Contact';
 
 const Component = styled(Box)`
   padding: 10px;
@@ -59,8 +58,6 @@ const Home = () => {
           timer={false}
           multi={true}
         />
-
-        {/* <Contact></Contact> */}
       </Component>
     </>
   )
